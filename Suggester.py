6d07559f69926fadc5f1f5f3697522174ad27894
Creator Hub <abinskaria2802@gmail.com>
2025-08-29 22:28:34 +0530

    fix: Update CSV file handling in Suggester.py to include UTF-8 encoding and simplify output formatting

--- Suggester.py
+++ Suggester.py
@@ -5,8 +5,8 @@ def Suggesters(input):
  # Take user input
  user_input = input
 
- # Open the CSV file
- with open('Remedies.csv', 'r') as file:
+ # Open the CSV file with proper encoding
+ with open('Remedies.csv', 'r', encoding='utf-8', errors='ignore') as file:
     # Create a CSV reader
     reader = csv.reader(file)
     rows = list(reader)
@@ -15,10 +15,8 @@ def Suggesters(input):
     output_value = None
     for row in rows:
         if row[0] == user_input:
-            # Each cell is Formated on the Particular Row
-            formatted_row = [cell.replace('', '') for cell in row]
             # Each cell is separated by \n\n\n
-            output_value = '\n\n\n'.join(formatted_row)
+            output_value = '\n\n\n'.join(row)
             break
 
     return output_value
